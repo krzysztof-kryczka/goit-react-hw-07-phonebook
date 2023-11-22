4fa55e22f0d95d59e28bc4cc8a5775e97ac259b8
Krzysztof Kryczka <kw.kryczka@gmail.com>
2023-11-22 11:50:38 +0100

    updated paths, added hook useEffect which loads contact list

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,18 @@
-import { ContactList } from './ContactList/ContactList';
-import { ContactForm } from './ContactForm/ContactForm';
-import { Filter } from './Filter/Filter';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { getContacts } from '#services/api';
+
+import { ContactList } from '#components/ContactList/ContactList';
+import { ContactForm } from '#components/ContactForm/ContactForm';
+import { Filter } from '#components/Filter/Filter';
 
 const App = () => {
-  
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getContacts());
+  }, [dispatch]);
+
   return (
     <>
       <ContactForm />
